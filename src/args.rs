6dcb0bdd1671cc9674a8b398ba0d3083176d261c
Maximilian Wende <dasisdormax@mailbox.org>
2022-03-10 15:01:39 +0100

    Implement mklink

--- src/args.rs
+++ src/args.rs
@@ -3,15 +3,29 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version)]
 pub struct Args {
-    /// Indicates that the file points to a directory instead of a regular file
+    /// Indicates that the link points to a directory instead of a regular file
     #[clap(short='d')]
-    is_directory: bool,
+    flag_dir: bool,
 
-    /// The location the link points to
+    /// The name of the symbolic link
+    link: String,
+
+    /// The path that the link points to
     /// 
     /// Can either be an absolute path or relative to the directory containing the link.
     target: String,
+}
 
-    /// The location the link is created at
-    link: String,
+impl Args {
+    pub fn is_directory(&self) -> bool {
+        self.flag_dir
+    }
+
+    pub fn get_target(&self) -> &str {
+        &self.target
+    }
+
+    pub fn get_link(&self) -> &str {
+        &self.link
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 mod args;
-use clap::Parser;
+use std::os::windows::prelude::OsStrExt;
 
+use clap::Parser;
+use winapi::um::winbase::CreateSymbolicLinkW;
+use std::iter::once;
+use std::ffi::OsStr;
+
+fn to_unicode<T: AsRef<OsStr>>(s: T) -> Vec<u16> {
+    OsStr::new(&s).encode_wide().chain(once(0)).collect()
+}
+
+const SYMBOLIC_LINK_FLAG_FILE: u32 = 0x0;
+const SYMBOLIC_LINK_FLAG_DIRECTORY: u32 = 0x1;
+const SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE: u32 = 0x2;
 fn main() {
     let args = args::Args::parse();
+    let symlink_file_name: Vec<u16> = to_unicode(args.get_link());
+    let target_file_name = to_unicode(args.get_target());
+
+    let mut flags = 0u32;
+    flags |= SYMBOLIC_LINK_FLAG_ALLOW_UNPRIVILEGED_CREATE;
+    flags |= if(args.is_directory()) {
+        SYMBOLIC_LINK_FLAG_DIRECTORY
+    } else {
+        SYMBOLIC_LINK_FLAG_FILE
+    };
+    unsafe {
+        CreateSymbolicLinkW(
+            symlink_file_name.as_ptr(), 
+            target_file_name.as_ptr(), 
+            flags
+        );
+    }
 }
